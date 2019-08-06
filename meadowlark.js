4841889798ebccdde599b685f992ccfd9b3b2ee3
mxydl2009 <mxydl2009@163.com>
2019-08-06 16:08:53 +0800

    Initial Commit

---
+++ meadowlark.js
@@ -0,0 +1,46 @@
+// 入口文件
+let express = require('express');
+// 创建应用实例
+let app = express();
+// 引入handlebars视图引擎，create()方法创建handlebars实例来注册应用的引擎
+let handlebars = require('express-handlebars').create({defaultLayout: 'main'});
+app.engine('handlebars', handlebars.engine);
+app.set('view engine', 'handlebars');
+
+app.set('port', process.env.PORT || 3000);
+
+let fortunes = [
+	"Conquer your fears or they will conquer you.",
+	'Rivers need spring.',
+	"Do not fear what you don't know.",
+	'You will have a pleasant surprise.',
+	'Whenever possible, keep it simple.'
+];
+
+app.use(express.static('public'));
+
+app.get('/', (req, res) => {
+	res.render('home');
+})
+
+app.get('/about', (req, res) => {
+	let randomFortune = fortunes[Math.floor(Math.random() * fortunes.length)];
+	res.render('about', {fortune: randomFortune});
+})
+
+app.use(function (req, res, next) {
+	res.status(404);
+	res.render('404');
+})
+
+app.use(function (err, req, res, next) {
+	console.error(err.stack);
+	res.type('text/plain');
+	res.status(500);
+	res.render('500');
+})
+
+app.listen(app.get('port'), function () {
+	console.log('Express started on http://localhost:' + app.get('port') + '; press Ctrl - C to terminate.');
+})
+
